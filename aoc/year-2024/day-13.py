fd59de737ac5d409c6ed0ede2174e500f10786db
James Green <jamestadleygreen@outlook.com>
2024-12-14 12:29:10 +0000

    feat: using maths

--- aoc/year-2024/day-13.py
+++ aoc/year-2024/day-13.py
@@ -1,33 +1,25 @@
 import re
+import math
+import numpy as np
 
 
-def parse_input(inp: str) -> list[tuple[int, int, int]]:
+def parse_input(inp: str) -> list[tuple[np.array, np.array]]:
     output = []
-    for block in inp.split("\n\n"):
-        a, b, target = block.split("\n")
-        a_s = re.search(r"X\+(\d+), Y\+(\d+)", a)
-        b_s = re.search(r"X\+(\d+), Y\+(\d+)", b)
-        target_s = re.search(r"X=(\d+), Y=(\d+)", target)
-        output.append(
-            (
-                (int(a_s.group(1)), int(a_s.group(2))),
-                (int(b_s.group(1)), int(b_s.group(2))),
-                (int(target_s.group(1)), int(target_s.group(2))),
-            )
-        )
+    numbers = [int(i) for i in re.findall(r"(\d+)", inp)]
+    for i in range(0, len(numbers), 6):
+        a = numbers[i], numbers[i + 2]
+        b = numbers[i + 1], numbers[i + 3]
+        target = numbers[i + 4], numbers[i + 5]
+        output.append((np.array([a, b]), np.array(target)))
     return output
 
 
-def calculate_tokens(target, b, a):
-    tokens = 0
-    while target[0] > 0 and target[1] > 0:
-        if target[0] % a[0] == 0:
-            remaining_tokens = target[0] // a[0]
-            if target[1] - a[1] * remaining_tokens == 0:
-                print(tokens, remaining_tokens)
-                return tokens + remaining_tokens
-        target = (target[0] - b[0], target[1] - b[1])
-        tokens += 3
+def calculate_tokens(target: np.array, buttons: np.array) -> np.array:
+    buttons_inv = np.linalg.inv(buttons)
+    button_presses = np.matmul(buttons_inv, target)
+    tickets = np.matmul([3, 1], button_presses)
+    if all(map(lambda x: math.isclose(x - round(x), 0, abs_tol=0.001), button_presses)):
+        return round(tickets)
     return 0
 
 
@@ -35,8 +27,15 @@ def part1(inp: str) -> int:
     machines = parse_input(inp)
     tokens = 0
     for machine in machines:
-        a, b, target = machine
-        print(target)
-        tokens += calculate_tokens(target, a, b)
-        print(tokens)
+        buttons, target = machine
+        tokens += calculate_tokens(target, buttons)
+    return tokens
+
+
+def part2(inp: str) -> int:
+    machines = parse_input(inp)
+    tokens = 0
+    for i, machine in enumerate(machines):
+        buttons, target = machine
+        tokens += calculate_tokens(target + 10_000_000_000_000, buttons)
     return tokens
